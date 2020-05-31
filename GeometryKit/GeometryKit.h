f46eab12632d60022786b86e9b9efd91f20d3e93
S. M. Hasibur Rahman <hasib.jhd@gmail.com>
2020-05-31 15:06:03 +0600

    Initial Commit

---
+++ GeometryKit/GeometryKit.h
@@ -0,0 +1,19 @@
+//
+//  GeometryKit.h
+//  GeometryKit
+//
+//  Created by S. M. Hasibur Rahman on 5/31/20.
+//  Copyright © 2020 S. M. Hasibur Rahman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeometryKit.
+FOUNDATION_EXPORT double GeometryKitVersionNumber;
+
+//! Project version string for GeometryKit.
+FOUNDATION_EXPORT const unsigned char GeometryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeometryKit/PublicHeader.h>
+
+
